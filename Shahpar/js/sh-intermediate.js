67eaff49f2ca85ca3bbf2f2c3aae9125a6d28ffd
shahparshabani <shahpar.shabani@g-experts.net>
2021-09-22 13:50:11 +0330

    format js

--- Shahpar/js/sh-intermediate.js
+++ Shahpar/js/sh-intermediate.js
@@ -3,24 +3,19 @@ document.write("Exercise 1: Sorting an array <br> <br>");
 
 var carType = "Tesla, Audi, Renault, Volvo, Mazda, Fiat, Ferrari";
 
-
-carTypeArray = carType.split(',');
+carTypeArray = carType.split(",");
 document.write(carTypeArray + "<br><br>");
 
 document.write("Sorted Array is: <br>" + carTypeArray.sort() + "<hr><br>");
 
-
-
-
-
 // Exercise 2-1
 document.write("Exercise 2-1 <br><br>");
 
-let fruit = 'apple, banana, kiwi'
-let fruitArr = fruit.split(',');
+let fruit = "apple, banana, kiwi";
+let fruitArr = fruit.split(",");
 document.write("Original fruit array: <br>" + fruitArr + "<br><br>");
 
-fruitArr.push('orange');
+fruitArr.push("orange");
 document.write(fruitArr + "<br>");
 
 fruitArr.pop();
@@ -29,8 +24,8 @@ document.write(fruitArr + "<hr><br>");
 // Exercise 2-2
 document.write("Exercise 2-2 <br><br>");
 
-let animal = 'monkey, horse, dog'
-let animalArr = animal.split(',');
+let animal = "monkey, horse, dog";
+let animalArr = animal.split(",");
 document.write("Original animal array: <br>" + animalArr + "<br><br>");
 
 animalArr.sort();
@@ -40,19 +35,18 @@ animalArr.unshift("cat");
 document.write(animalArr + "<br><br>");
 
 // Add space at the beggining of the last element if there is no space.
-if (animalArr[animalArr.length -1].charAt(0) != " ") {
-    animalArr[animalArr.length -1] = " " + animalArr[animalArr.length -1];
+if (animalArr[animalArr.length - 1].charAt(0) != " ") {
+  animalArr[animalArr.length - 1] = " " + animalArr[animalArr.length - 1];
 }
 document.write(animalArr + "<br><br><hr>");
-
 
 // Exercise 3
 document.write("Exercise 3: <br> <br>");
 
 let fruitS = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
 
-let fruitA = fruitS.split('/');
+let fruitA = fruitS.split("/");
 
-for(let i=0; i<fruitA.length-1 ; i++) {
-    document.write(fruitA[i] + "<br>");
+for (let i = 0; i < fruitA.length - 1; i++) {
+  document.write(fruitA[i] + "<br>");
 }
